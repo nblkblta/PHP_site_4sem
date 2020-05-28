f11e7ebd5849a0b1636a6d6d6bdfcd4b7fcb2b09
nblkblta <nblkdjum@gmail.com>
2020-05-28 18:42:35 +1000

    add avatar to comments
    Добавил аватарки в комментарии

--- get_comments.php
+++ get_comments.php
@@ -2,21 +2,30 @@
 include 'db.php';
 $id_post = $_GET['id_post'];
 $result = $mysql->query("SELECT * FROM `comments` WHERE  `id_post` ='$id_post' ");
-$mysql->close();
+
 ?>
 <body>
 	<?
 while(($row=$result->fetch_assoc())!=false):
 ?>
-
+	<? $name = $row["id_user"];
+			$res=$mysql->query("SELECT * FROM `users` WHERE `id`='$name'");
+			$r=$res->fetch_assoc();?>
     </br>
 	<div class="com">
-	<p class="zagolovok"><?echo $row['name'];?></p>	
+		<div class='user_foto'>
+		<img  class="cont" src=<?echo $r['avatar'];?>>
+		</div>
+	<p class="zagolovok"><div class='blogname'>
+			
+			<?echo $r['name'];?>
+		</div>
+		 </p>	
 
 	<p class="zagolovok1"><?echo $row['date'];?></p>
 
 	<p class="blogtext1"><?echo $row['text'];?></p>
-	<? if(($row['name'] ==$_COOKIE['name'])||($_COOKIE['status']>1)): ?>
+	<? if(($row['name'] == $_COOKIE['name'])||($_COOKIE['status']>1)): ?>
 		<form style="display: inline-block;margin-left: 80%; margin-top: 2%;" name="red" action="redact_comment.php"  method="post">
 		<input type="hidden" name="id_post" value=<?echo $id_post ?>>
   		<input type="hidden" name="id" value=<?echo $row['id'] ?>>
@@ -34,3 +43,6 @@ endwhile;
 ?>
 
 </body>
+<?
+$mysql->close();
+?>
